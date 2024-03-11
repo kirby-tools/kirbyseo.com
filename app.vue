69449cb4dc3c86d5fd13d0e097069e619fb6e803
Johann Schopplich <mail@johannschopplich.com>
2024-03-11 07:55:14 +0100

    chore: add conversion script

--- app.vue
+++ app.vue
@@ -26,6 +26,18 @@ useServerHead({
       defer: true,
       "data-domain": "kirbyseo.com",
     },
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=AW-16488150310",
+      async: true,
+    },
+    {
+      innerHTML: `
+window.dataLayer = window.dataLayer || []
+function gtag() { dataLayer.push(arguments) }
+gtag('js', new Date())
+gtag('config', 'AW-16488150310')
+      `.trimStart(),
+    },
   ],
 });
 
