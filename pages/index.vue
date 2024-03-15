b95830b24c46b7d601ff2baefc33118109eb6060
Johann Schopplich <mail@johannschopplich.com>
2024-03-15 14:44:39 +0100

    feat: autoplay videos

--- pages/index.vue
+++ pages/index.vue
@@ -20,21 +20,6 @@ useSeoMeta({
   ogImage: "https://kirbyseo.com/social-card.png",
   twitterImage: "https://kirbyseo.com/social-card.png",
 });
-
-const isPlaying = ref(false);
-
-function handleVideoClick(event: MouseEvent) {
-  const player = event.target as HTMLVideoElement;
-
-  if (isPlaying.value) {
-    player.pause();
-    isPlaying.value = false;
-    return;
-  }
-
-  player.play();
-  isPlaying.value = true;
-}
 </script>
 
 <template>
@@ -67,39 +52,15 @@ function handleVideoClick(event: MouseEvent) {
       </template>
 
       <template #title>
-        <span v-html="page.hero?.title" />
+        <span v-html="page.hero.title" />
       </template>
 
       <template #description>
-        <span v-html="page.hero?.description" />
+        <span v-html="page.hero.description" />
       </template>
 
       <template #default>
-        <div
-          class="group relative cursor-pointer rounded-xl shadow ring-1 ring-gray-200 dark:ring-gray-800"
-          :class="[
-            !isPlaying && 'hover:ring-primary-500 dark:hover:ring-primary-400',
-          ]"
-        >
-          <video
-            src="/vid/kirby-seo-audit-demo.mp4"
-            poster="/vid/kirby-seo-audit-demo-poster.jpg"
-            muted
-            class="rounded-xl"
-            @ended="isPlaying = false"
-            @click="handleVideoClick"
-          />
-
-          <div
-            v-show="!isPlaying"
-            class="pointer-events-none absolute inset-0 flex items-center justify-center rounded-xl bg-gradient-to-b from-gray-950/50 to-[rgba(0,0,0,0)] to-20%"
-          >
-            <UIcon
-              name="i-ri-play-circle-fill"
-              class="group-hover:text-primary-600 h-[4rem] w-[4rem] text-gray-900 md:h-[6rem] md:w-[6rem]"
-            />
-          </div>
-        </div>
+        <HomeVideo v-bind="page.hero.video" />
       </template>
     </ULandingHero>
 
