924b1bee20e7b2cfe8036dd68f84a605fc1c7ad1
Johann Schopplich <mail@johannschopplich.com>
2025-01-01 21:43:33 +0100

    feat: discount banner

--- pages/buy.vue
+++ pages/buy.vue
@@ -61,55 +61,68 @@ defineOgImageComponent("Default", {
             </div>
           </div>
 
-          <UPricingCard
-            v-bind="page.plan"
-            :button="{ ...page.plan.button, to: checkoutUrl }"
-            :ui="{
-              highlight: 'ring-1 ring-primary-600 dark:ring-primary-200',
-            }"
-          >
-            <template #features>
-              <ul
-                v-if="page.plan.features?.length"
-                class="mb-6 space-y-3 text-sm"
+          <div class="relative">
+            <UPricingCard
+              v-bind="page.plan"
+              :button="{ ...page.plan.button, to: checkoutUrl }"
+              :ui="{
+                highlight: 'ring-1 ring-primary-600 dark:ring-primary-200',
+              }"
+            >
+              <template #features>
+                <ul
+                  v-if="page.plan.features?.length"
+                  class="mb-6 space-y-3 text-sm"
+                >
+                  <li
+                    v-for="(offer, index) of page.plan.features"
+                    :key="index"
+                    class="flex min-w-0 items-center gap-x-3"
+                  >
+                    <UIcon
+                      :name="appConfig.ui.icons.check"
+                      class="text-primary h-5 w-5 flex-shrink-0"
+                    />
+
+                    <span class="truncate text-gray-600 dark:text-gray-400">{{
+                      offer
+                    }}</span>
+                  </li>
+                </ul>
+
+                <UDivider label="Licensee" class="mb-6" />
+
+                <UInput
+                  v-model="licenseHolder"
+                  color="gray"
+                  placeholder="License holder"
+                  class="mb-2"
+                />
+                <div class="text-sm text-gray-500 dark:text-gray-400">
+                  Who will own this license (e.g. your full name, organization,
+                  or client)? Will be you by default.
+                </div>
+              </template>
+            </UPricingCard>
+
+            <div
+              class="absolute inset-x-0 top-0 flex translate-y-[-50%] justify-center"
+            >
+              <UButton
+                size="2xs"
+                variant="outline"
+                to="https://hub.kirby.tools"
+                :ui="{
+                  rounded: 'rounded-full',
+                  variant: {
+                    outline: 'bg-white dark:bg-gray-900',
+                  },
+                }"
               >
-                <li
-                  v-for="(offer, index) of page.plan.features"
-                  :key="index"
-                  class="flex min-w-0 items-center gap-x-3"
-                >
-                  <UIcon
-                    :name="appConfig.ui.icons.check"
-                    class="text-primary h-5 w-5 flex-shrink-0"
-                  />
-
-                  <span class="truncate text-gray-600 dark:text-gray-400">{{
-                    offer
-                  }}</span>
-                </li>
-              </ul>
-
-              <UDivider label="Licensee" class="mb-6" />
-
-              <UInput
-                v-model="licenseHolder"
-                color="gray"
-                placeholder="License holder"
-                class="mb-2"
-              />
-              <div class="text-sm text-gray-500 dark:text-gray-400">
-                Who will own this license (e.g. your full name, organization, or
-                client)? Will be you if left empty. All licenses are managed in
-                <a
-                  href="https://hub.kirby.tools"
-                  class="hover:text-primary-500 decoration-primary-500 font-medium underline"
-                  target="_blank"
-                >
-                  hub.kirby.tools</a
-                >.
-              </div>
-            </template>
-          </UPricingCard>
+                50% off for Returning Customers
+              </UButton>
+            </div>
+          </div>
         </div>
 
         <div>
